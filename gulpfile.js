6782eb81cd38c585f7314f7ca1be706ca700e3bd
Erik Fisher <ebenfishy@gmail.com>
2020-08-11 00:04:34 -0700

    Modified gulpfile to be more modern for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -16,22 +16,40 @@ var SCSS_SRC = './src/Assets/scss/**/*.scss';
 var SCSS_DEST = './src/Assets/css';
 
 // Compile SCSS_SS
-gulp.task('compile_scss', function() {
+// gulp.task('compile_scss', function() {
+//
+//   return gulp.src(SCSS_SRC)
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(minifyCSS())
+//     .pipe(rename({ suffix: '.min' }))
+//     .pipe(changed(SCSS_DEST))
+//     .pipe(gulp.dest(SCSS_DEST));
+//
+// });
 
-  return gulp.src(SCSS_SRC)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(changed(SCSS_DEST))
-    .pipe(gulp.dest(SCSS_DEST));
-
-});
+function compile_scss() {
+    return gulp.src(SCSS_SRC)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(changed(SCSS_DEST))
+        .pipe(gulp.dest(SCSS_DEST));
+}
 
 
 // detect changes in SCSS
-gulp.task('watch_scss', function() {
-  gulp.watch(SCSS_SRC, gulp.series('compile_scss'));
-});
+// gulp.task('watch_scss', function() {
+//   gulp.watch(SCSS_SRC, gulp.series('compile_scss'));
+// });
+
+function watch_scss() {
+    gulp.watch(SCSS_SRC, compile_scss);
+}
 
 // Run tasks
-gulp.task('default', gulp.series('watch_scss'));
+// gulp.task('default', gulp.series('watch_scss'));
+
+gulp.task('default', watch_scss);
+
+exports.compile_scss = compile_scss;
+exports.watch_scss = watch_scss;
